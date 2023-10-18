d2850377be8621ca1c76dd7a05a8a6c83dacaefb
SebasHS <20190943@aloe.ulima.edu.pe>
2023-10-18 16:08:18 -0500

    terminado

---
+++ Assets/Scripts/Dialogo.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Dialogo : MonoBehaviour
+{
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+
+    private float typingTime = 0.05f;
+    private void Start()
+    {
+        dialoguePanel.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            else if(dialogueText.text == dialogueLines[lineIndex]){
+                NextDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Debug.Log("Iniciar dialogo");
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            Debug.Log("No debe iniciar dialogo");
+        }
+    }
+}
